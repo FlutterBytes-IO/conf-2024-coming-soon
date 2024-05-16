1024c9c68390e68d09bf5169a2fce73d730751e8
DrPrime01 <gabrielshoyombo2002@gmail.com>
2024-05-16 20:28:44 +0100

    fix: form modal success message

--- src/components/GetNotifiedForm.tsx
+++ src/components/GetNotifiedForm.tsx
@@ -7,7 +7,7 @@ import { DarkmodeContext } from "@/Context/DarkMode";
 import Button from "./Button";
 import TickIcon from "./vectors/TickIcon"
 
-export default function GetNotifiedForm({ closeModal }: { closeModal?: () => void;}) {
+export default function GetNotifiedForm({ closeModal }: { closeModal?: () => void; }) {
     const { isDarkmode } = useContext(DarkmodeContext)
     const [value, setValue] = useState("");
     const [isSubmitted, setIsSubmitted] = useState(false);
@@ -31,8 +31,8 @@ export default function GetNotifiedForm({ closeModal }: { closeModal?: () => voi
             if (!res.ok) {
                 throw new Error(`Error: ${res.status}`);
             }
-            const data = await res.json();
-            toast.success(data?.message)
+            await res.json();
+            toast.success("Hooray! Your email was added successfully!")
             setIsSubmitted(true)
             setValue("")
         } catch (err) {
@@ -46,10 +46,10 @@ export default function GetNotifiedForm({ closeModal }: { closeModal?: () => voi
     return (
         <form onSubmit={handleSubmit} className={`flex flex-col gap-y-3 bg-transparent md:flex-row md:gap-y-0 transition-all duration-500 items-center rounded-[30px] md:!border ${isDarkmode ? "md:!border-[#1167B1]" : "md:!border-[#D0EFFF] md:hover:!border md:hover:!border-[#1167B1] md:!bg-[#E5F6FF]"}`}>
             <input
-            value={value}
-            placeholder="Enter email address"
-            onChange={(e) => setValue(e.target.value)}
-            className={`md:py-4 md:text-start py-[13.5px] outline-[#1167B1] md:w-full md:pl-5 flex-1 pr-0 md:outline-none text-[#2A9DF4] placeholder:text-[#2A9DF4] placeholder:italic text-xs md:text-sm bg-transparent ${isDarkmode ? "border border-[#1167B1] md:border-none w-full !pl-4 !rounded-[30px] md:rounded-none" : "!bg-[#E5F6FF] text-center w-full !pl-0 md:!pl-5 !rounded-[30px]"}`}
+                value={value}
+                placeholder="Enter email address"
+                onChange={(e) => setValue(e.target.value)}
+                className={`md:py-4 md:text-start py-[13.5px] outline-[#1167B1] md:w-full md:pl-5 flex-1 pr-0 md:outline-none text-[#2A9DF4] placeholder:text-[#2A9DF4] placeholder:italic text-xs md:text-sm bg-transparent ${isDarkmode ? "border border-[#1167B1] md:border-none w-full !pl-4 !rounded-[30px] md:rounded-none" : "!bg-[#E5F6FF] text-center w-full !pl-0 md:!pl-5 !rounded-[30px]"}`}
             />
             <Button type="submit" otherstyles={`py-4 px-5 flex items-center justify-center text-center gap-x-1 w-full md:w-auto`}>{isSubmitted ? <>Email Added <TickIcon /></> : "Get Notified"}</Button>
         </form>
